904a3e9322adc0662b0e65dc74ec0f821965cd55
Alexandr Podolin <podolin.alexandr@gmail.com>
2020-04-23 16:00:13 +0300

    Added messages on phone screen Added Log.d

--- app/src/main/java/com/example/androidactivitylivecycle/MainActivity.java
+++ app/src/main/java/com/example/androidactivitylivecycle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidactivitylivecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,31 +16,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        
+        Log.d("LifeCycle :", "onCreate" );
+        textView.append("onCreate" + "\n");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+        Log.d("LifeCycle :", "onStart" );
+        textView.append("onStart" + "\n");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("LifeCycle :", "onResume" );
+        textView.append("onResume" + "\n");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Log.d("LifeCycle :", "onPause" );
+        textView.append("onPause" + "\n");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Log.d("LifeCycle :", "onStop" );
+        textView.append("onStop" + "\n");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("LifeCycle :", "onDestroy" );
+        textView.append("onDestroy" + "\n");
     }
 }
